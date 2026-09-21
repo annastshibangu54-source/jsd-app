0ba32bb3249d33c818008fe23d884d61722569bf
annastshibangu54-source <annastshibangu54@gmail.com>
2026-09-21 02:04:42 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 // Configuration de la base de données PostgreSQL Cloud (Render)
-$host     = "dpg-d1234567890-a"; // Remplacez par votre Host Render interne/externe
+$host     = "dpg-dao54i8473hc73bfmfng-a"; // Remplacez par votre Host Render interne/externe
 $port     = "5432";
 $dbname   = "jsd_db";           // Remplacez par le nom de votre DB Render
 $user     = "jsd_user";         // Remplacez par votre User Render
-$password = "votre_mot_de_passe"; // Remplacez par votre Password Render
+$password = "fEPwCcF3c3XE6WYc8hOdXoBOQLXWUlfj"; // Remplacez par votre Password Render
 
 try {
     $dsn = "pgsql:host=$host;port=$port;dbname=$dbname;";
